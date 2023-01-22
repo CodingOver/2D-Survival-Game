85ec19544d34f2e516e27c5cc86260415c2a61ab
CodingOver <paypal1business.over@gmail.com>
2023-01-22 23:10:07 +0100

    create a player

---
+++ index.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext("2d")
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+// Create Player
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "red")
+player.draw()
+
+console.log(player)
