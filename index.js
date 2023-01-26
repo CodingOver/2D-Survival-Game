95d31faed98c3bab0a69640471aa8cbc53fb1b2e
CodingOver <paypal1business.over@gmail.com>
2023-01-26 23:02:48 +0100

    Create a Shoot Projectiles

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 
-// Create Player
+// Create a  Player
 class Player {
     constructor(x, y, radius, color) {
         this.x = x;
@@ -18,14 +18,58 @@ class Player {
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+// Create a Shoot Projectiles
+class Projectiles {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
         ctx.fill()
+    }
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-
 const player = new Player(x, y, 30, "red")
-player.draw()
+const projectiles = [];
 
-console.log(player)
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach((projectile) => {
+        projectile.update()
+    })
+}
+
+addEventListener("click", (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(
+        new Projectiles(canvas.width / 2, canvas.height / 2, 5, "blue", velocity)
+    )
+})
+
+
+animate()
