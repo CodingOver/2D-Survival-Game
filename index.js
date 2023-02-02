6896c86aa2e6e63f686680f0d638ea435dc58e39
CodingOver <paypal1business.over@gmail.com>
2023-02-02 23:17:27 +0100

    Colorizing Game

--- index.js
+++ index.js
@@ -67,13 +67,13 @@ class Enemy {
 }
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-const player = new Player(x, y, 30, "red")
+const player = new Player(x, y, 10, "white")
 const projectiles = [];
 const enemies = [];
 
 function spawnEnemies() {
     setInterval(() => {
-        const radius = Math.random() * (30 - 10)
+        const radius = Math.random() * (50)
 
         let x, y;
         if (Math.random() < 0.5) {
@@ -83,7 +83,8 @@ function spawnEnemies() {
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = "black"
+        // colorizing Game
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle = Math.atan2(
             canvas.height / 2 - y, canvas.width / 2 - x)
 
@@ -101,11 +102,24 @@ function spawnEnemies() {
 let animationId;
 function animate() {
     animationId = requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
 
-    projectiles.forEach((projectile) => {
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+
+        // Remove Projectile From Edges Of Screen
+        if (
+            projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y + projectile.radius > canvas.height
+        ) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
 
     enemies.forEach((enemy, index) => {
@@ -117,7 +131,7 @@ function animate() {
             cancelAnimationFrame(animationId)
         }
 
-        // Detect collision on enemy & prjectile // hit
+        // Detect collision on enemy & projectile // hit
         projectiles.forEach((projectile, projectileIndex) => {
             const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
@@ -137,11 +151,11 @@ addEventListener("click", (event) => {
         event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
 
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 6,
+        y: Math.sin(angle) * 6
     }
     projectiles.push(
-        new Projectiles(canvas.width / 2, canvas.height / 2, 5, "blue", velocity)
+        new Projectiles(canvas.width / 2, canvas.height / 2, 5, "white", velocity)
     )
 })
 animate()
