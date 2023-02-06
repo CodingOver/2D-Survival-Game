cda1939e7b2e6cebb1859947cf5d2038903e7095
CodingOver <paypal1business.over@gmail.com>
2023-02-06 23:59:31 +0100

    Add HomingSpinning Effect

--- index.js
+++ index.js
@@ -78,18 +78,19 @@ class Enemy {
         this.radius = radius
         this.color = color
         this.velocity = velocity
-        this.type = 'linear'
+        this.type = 'homingSpinning'
         this.center = {
             x,
             y
         }
         this.radians = 0
-        if (Math.random() < 0.25) {
-            this.type = "homing"
-            if (Math.random() < .5) {
-                this.type = "spinning"
-            }
-        }
+
+        // if (Math.random() < 0.25) {
+        //     this.type = "homing"
+        //     if (Math.random() < .5) {
+        //         this.type = "spinning"
+        //     }
+        // }
     }
     draw() {
         ctx.beginPath();
@@ -121,6 +122,23 @@ class Enemy {
 
             this.x = this.center.x + Math.cos(this.radians) * 100
             this.y = this.center.y + Math.sin(this.radians) * 100
+        } else if (this.type === "homingSpinning") {
+            const angle = Math.atan2(player.y - this.y, player.x - this.x)
+
+            this.velocity = {
+                x: Math.cos(angle),
+                y: Math.sin(angle)
+            }
+            this.x = this.x + this.velocity.x;
+            this.y = this.y + this.velocity.y;
+
+            this.radians += 0.05
+            this.center.x += this.velocity.x;
+            this.center.y += this.velocity.y;
+
+            this.x = this.center.x + Math.cos(this.radians) * 100
+            this.y = this.center.y + Math.sin(this.radians) * 100
+
         }
     }
 
